003c62a91358a15596c56200b3dce559654da01d
EkaterinaShamshina <nireta2009@yandex.ru>
2022-05-30 22:44:17 +0300

    custom select was added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import 'normalize.css'
 import './styles/main.scss';
 import './styles/tooltip.scss';
+import './styles/customselect.scss';
+
 import customSelect from 'custom-select';
+
+customSelect('.mySelect');
