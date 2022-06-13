b4fc0b18fcdbef344b24b6e5ed15aa7585b8c72c
EkaterinaShamshina <nireta2009@yandex.ru>
2022-06-13 11:55:07 +0300

    smooth scroll was added

--- src/index.js
+++ src/index.js
@@ -77,3 +77,21 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
+
+$(document).ready(function(){
+    // = Вешаем событие прокрутки к нужному месту
+    	//	 на все ссылки якорь которых начинается на #
+    	$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    		e.preventDefault();
+    
+    		var target = this.hash,
+    		$target = $(target);
+    
+    		$('html, body').stop().animate({
+    			'scrollTop': $target.offset().top - 200
+    		}, 900, 'swing', function () {
+    			window.location.hash = target;
+    		});
+    	});
+    
+    });
